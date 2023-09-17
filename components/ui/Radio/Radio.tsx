af1f39740745db123f0e4a573cc978b5f81094c8
Yehor Bilokrynytskyi <>
2023-09-17 23:05:38 +0300

    Checkmark added to checkbox

--- components/ui/Radio/Radio.tsx
+++ components/ui/Radio/Radio.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import { CheckIcon } from "@heroicons/react/24/solid";
 
 interface RadioProps {
   label: string;
@@ -8,6 +9,7 @@ interface RadioProps {
   onChange: (value: string) => void;
 }
 
+// TODO: rename
 export const Radio: React.FC<RadioProps> = ({
   label,
   name,
@@ -16,13 +18,27 @@ export const Radio: React.FC<RadioProps> = ({
   onChange,
 }) => {
   const customRadioClassNames = clsx(
-    "bg-white text-black p-5 flex justify-center transition hover:opacity-90 rounded-md",
+    "bg-white text-black p-5 flex justify-center transition hover:opacity-90 rounded-md relative",
     checked && "ring-4 ring-green-500 text-green-500"
   );
 
+  const optionCheckboxClassNames = clsx(
+    "border border-black w-4 h-4 absolute right-5 top-1/2 -mt-2 rounded-sm flex items-center justify-center",
+    checked && "border-green-500"
+  );
+
+  const isCheckbox = type === "checkbox";
+
   return (
     <label className="cursor-pointer">
-      <div className={customRadioClassNames}>{label}</div>
+      <div className={customRadioClassNames}>
+        {label}
+        {isCheckbox && (
+          <div className={optionCheckboxClassNames}>
+            {checked && <CheckIcon className="h-w-3/4 w-3/4" />}
+          </div>
+        )}
+      </div>
       <input
         name={name}
         className="sr-only"
